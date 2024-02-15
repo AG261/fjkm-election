932ea08393a6188b7fe2b9229f726cbf9e6de16f
Lova RABARISON <lrabarison@myagency.mg>
2024-02-15 14:42:05 +0300

    FO : optimisation résultat

--- src/Controller/ResultController.php
+++ src/Controller/ResultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Manager\CandidatManager;
 use App\Manager\VoteManager;
 use App\Repository\Voting\VoteResultRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -12,10 +13,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ResultController extends AbstractController
 {
     #[Route('/', name: 'app_result')]
-    public function index(): Response
-    {
+    public function index(CandidatManager $candidatManager): Response
+    {   
+        $candidats  = $candidatManager->getCandidatCount();
+        
         return $this->render('Result/index.html.twig', [
-            'controller_name' => 'ResultController'
+            'controller_name' => 'ResultController',
+            'candidats'       => $candidats
         ]);
     }
 
