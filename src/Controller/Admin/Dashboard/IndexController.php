471394812b570a028f058c6e3ae50001fe9571c2
Lova RABARISON <lrabarison@myagency.mg>
2024-02-16 14:37:52 +0300

    ROLE : optimisation des redirection selon les roles

--- src/Controller/Admin/Dashboard/IndexController.php
+++ src/Controller/Admin/Dashboard/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin\Dashboard;
 
+use App\Common\Constants\UserConstants;
 use App\Manager\CandidatManager;
 use App\Manager\VoteManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -23,6 +24,17 @@ class IndexController extends AbstractController
     #[Route('/dashboard', name:'.dashboard')]
     public function index(Request $_request): Response
     {
+
+        $user  = $this->getUser() ;
+        $roles = $user->getRoles() ;
+        
+        if (in_array(UserConstants::USER_ROLE_OPERATOR, $roles) 
+            && (!in_array(UserConstants::USER_ROLE_ADMIN, $roles)
+            || !in_array(UserConstants::USER_ROLE_VALIDATOR, $roles))) {
+            $route = 'app_result' ;
+            return $this->redirectToRoute($route);
+        }
+        
         $canditats  = $this->_candidatManager->getCandidatCount();
         $votingCount = $this->_voteManager->getVotingCount();
         return $this->render('Admin/Dashboard/index.html.twig', [
