d1aed697e2b90b948007e6f4009be1cd686932cf
Lova RABARISON <lrabarison@myagency.mg>
2024-02-16 14:40:43 +0300

    ROLE : optimisation des redirection selon les roles

--- src/Controller/Admin/Dashboard/IndexController.php
+++ src/Controller/Admin/Dashboard/IndexController.php
@@ -28,7 +28,7 @@ class IndexController extends AbstractController
         $user  = $this->getUser() ;
         $roles = $user->getRoles() ;
         
-        if (in_array(UserConstants::USER_ROLE_ADMIN, $roles)) {
+        if (!in_array(UserConstants::USER_ROLE_ADMIN, $roles)) {
             $route = 'app_result' ;
             return $this->redirectToRoute($route);
         }
